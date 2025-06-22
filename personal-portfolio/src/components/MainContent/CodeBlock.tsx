9d4b2daeda2ddc60243c7ecb072b65e5a83a6d01
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-22 12:10:39 -0300

    feat: Adding prismjs

--- personal-portfolio/src/components/MainContent/CodeBlock.tsx
+++ personal-portfolio/src/components/MainContent/CodeBlock.tsx
@@ -1,3 +1,20 @@
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/components/prism-typescript.js";
+import "dracula-prism/dist/css/dracula-prism.css";
+
 export const CodeBlock = () => {
-  return <div className="w-full font-fira-code">CodeBlock</div>;
+  const code = "const teste : string = 'Testee'; \nlet age = 18;";
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [code]);
+
+  return (
+    <div className="w-full font-fira-code">
+      <pre className="language-typescript !m-0 !p-0">
+        <code className="language-typescript">{code}</code>
+      </pre>
+    </div>
+  );
 };
