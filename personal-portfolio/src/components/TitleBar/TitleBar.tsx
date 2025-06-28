6e3aa118d599d40e520630a27ac0dbbcaff2b60d
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-27 22:10:33 -0300

    feat:updating title bar

--- personal-portfolio/src/components/TitleBar/TitleBar.tsx
+++ personal-portfolio/src/components/TitleBar/TitleBar.tsx
@@ -1,7 +1,10 @@
 export const TitleBar = () => {
   return (
-    <header className="bg-slate-black text-center text-white text-sm font-medium">
+    <header className="bg-slate-black text-center text-white text-sm font-medium relative">
       <h1>personal-portfolio</h1>
+      <div className="absolute top-0 right-0">
+        <button className="rounded-full  bg-amber-600 block h-4 w-4"></button>
+      </div>
     </header>
   );
 };
