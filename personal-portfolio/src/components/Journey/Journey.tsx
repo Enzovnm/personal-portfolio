68fd68f6387773d44783507eb28711155d397e87
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-07-12 21:42:30 -0300

    feat: Adding card for journeys

--- personal-portfolio/src/components/Journey/Journey.tsx
+++ personal-portfolio/src/components/Journey/Journey.tsx
@@ -1,3 +1,28 @@
 export const Journey = () => {
-  return <div className="flex"></div>;
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="w-10/12 h-full flex justify-center ">
+        <ul className="before:content-[''] before:h-full before:w-1 relative before:absolute before:bg-pink-500">
+          <li className="absolute before:content-[''] before:bg-slate-blue-explorer before:w-3 before:h-8 before:-z-0 before:-left-2.5 bg- before:absolute left-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
+            <div className="flex items-center justify-between w-full">
+              <h1 className="text-xl font-bold">School: Giordano</h1>
+              <p>2019</p>
+            </div>
+            <div className="mt-4">
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ratione cum recusandae ad enim? Illum accusamus quas odio libero
+                vel! Recusandae natus voluptate modi quisquam accusantium
+                voluptas consequatur excepturi ratione doloremque!
+              </p>
+            </div>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
