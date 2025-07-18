ee27127c9bc9dfb1859ab39fad28449e4d640548
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-07-17 22:33:33 -0300

    feat: Adding more cards

--- personal-portfolio/src/components/Journey/Journey.tsx
+++ personal-portfolio/src/components/Journey/Journey.tsx
@@ -3,10 +3,10 @@ export const Journey = () => {
     <div className="w-full h-full flex justify-center">
       <div className="w-10/12 h-full flex justify-center ">
         <ul className="before:content-[''] before:h-full before:w-1 relative before:absolute before:bg-pink-500">
-          <li className="before:content-[''] before:border-r-[10px] before:border-b-[20px] before:border-l-[10px] before:border-t-transparent before:border-r-transparent before:border-b-slate-blue-explorer before:border-l-transparent before:-rotate-90 before:h-8 before:-z-0 before:absolute before:-left-3.5 absolute  left-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
+          <li className="before:content-[''] before:border-r-[10px] top-28 before:border-b-[20px] before:border-l-[10px] before:border-t-transparent before:border-r-transparent before:border-b-slate-blue-explorer before:border-l-transparent before:-rotate-90 before:h-8 before:-z-0 before:absolute before:-left-3.5 absolute  left-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
             <div className="flex items-center justify-between w-full">
               <h1 className="text-xl font-bold">School: Giordano</h1>
-              <p>2019</p>
+              <p>2021</p>
             </div>
             <div className="mt-4">
               <p className="text-justify">
@@ -17,9 +17,58 @@ export const Journey = () => {
               </p>
             </div>
           </li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li className="before:content-[''] before:border-r-[10px] before:border-b-[20px] before:border-l-[10px] before:border-t-transparent before:border-r-transparent before:border-b-slate-blue-explorer before:border-l-transparent before:rotate-90  before:h-8 before:-z-0 before:absolute before:-right-3.5 absolute  right-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
+            <div className="flex items-start justify-between w-full">
+              <h1 className="text-xl font-bold">
+                Technical School: <br />
+                ETEC Prof° Basilides de Godoy
+              </h1>
+              <p>2020~2021</p>
+            </div>
+            <div className="mt-4">
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ratione cum recusandae ad enim? Illum accusamus quas odio libero
+                vel! Recusandae natus voluptate modi quisquam accusantium
+                voluptas consequatur excepturi ratione doloremque!
+              </p>
+            </div>
+          </li>
+          <li className="before:content-[''] before:border-r-[10px] before:border-b-[20px] before:border-l-[10px] before:border-t-transparent before:border-r-transparent before:border-b-slate-blue-explorer before:border-l-transparent before:rotate-90  before:h-8 before:-z-0 before:absolute before:-right-3.5 absolute top-96 right-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
+            <div className="flex items-start justify-between w-full">
+              <h1 className="text-xl font-bold">
+                English language school : <br />
+                Wiseup
+              </h1>
+              <p>2019~2022</p>
+            </div>
+            <div className="mt-4">
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ratione cum recusandae ad enim? Illum accusamus quas odio libero
+                vel! Recusandae natus voluptate modi quisquam accusantium
+                voluptas consequatur excepturi ratione doloremque!
+              </p>
+            </div>
+          </li>
+          <li className="before:content-[''] before:border-r-[10px] top-[36rem] before:border-b-[20px] before:border-l-[10px] before:border-t-transparent before:border-r-transparent before:border-b-slate-blue-explorer before:border-l-transparent before:-rotate-90 before:h-8 before:-z-0 before:absolute before:-left-3.5 absolute  left-3.5 w-96 h-80 bg-slate-blue-explorer p-4 rounded-xl text-white">
+            <div className="flex items-start justify-between w-full">
+              <h1 className="text-xl font-bold">
+                College:
+                <br />
+                Universidade Paulista
+              </h1>
+              <p>2022 ~ Present</p>
+            </div>
+            <div className="mt-4">
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ratione cum recusandae ad enim? Illum accusamus quas odio libero
+                vel! Recusandae natus voluptate modi quisquam accusantium
+                voluptas consequatur excepturi ratione doloremque!
+              </p>
+            </div>
+          </li>
           <li></li>
         </ul>
       </div>
